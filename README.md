75ca61a1d15a3df6ddb3c09846799d2fca3df441
Mike <mike.zhang.619@gmail.com>
2019-04-10 15:09:07 -0400

    add README

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # sketch_to_coordinates
+
+Converts the lines in an image to a series of paths in a cartesian coordinate system. A manipulator can then draw the image by transforming the generated paths to the manipulator base frame and solving for the manipulator joint angles. 
--- generate_trajectory.m
+++ generate_trajectory.m
@@ -21,6 +21,10 @@ for i = 1:size(coord_paths,2)
     coord_paths{i} = [x_interp ; y_interp];
 end
 
+% step to sort path based to shorten unecessarily long travel times
+% i.e the next path to draw should be close to the current path end
+% location
+
 trajectory = stitchPath(coord_paths,x_offset,y_offset,z_move,z_draw);
 
 scatter3(trajectory(1,:),trajectory(2,:),trajectory(3,:),5)
